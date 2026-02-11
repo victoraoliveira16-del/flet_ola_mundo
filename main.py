14994848cce9d3856ab5482149f1c383b221047e
Victor Oliveira <victor.a.oliveira16@aluno.senai.br>
2026-02-11 14:20:01 -0300

    ola mundo pronto

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.title = "Meu Primeiro app Flet"
+    page.bgcolor = "#118df2"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.add(
         ft.Text(value="Victor"),
         ft.ElevatedButton('Clique aqui')
